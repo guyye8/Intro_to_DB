c3950b87ca3b3928d2768dc244ae4f24573d8d44
guyye8 <guyasau@gmail.com>
2025-06-29 21:22:57 +0300

    Add files via upload

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
